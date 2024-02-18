13b134fd158308cc1393d7cae11d60760cd70deb
Mohamed Ashour <boxv38145@gmail.com>
2024-02-18 23:12:31 +0200

    UserHomePage => middle section

--- src/components/pages/UserHomePage.tsx
+++ src/components/pages/UserHomePage.tsx
@@ -1,10 +1,75 @@
 import Sidebar from "../partials/Sidebar.tsx";
+import {FaRegCircleUser} from "react-icons/fa6";
+import {MdOutlinePermMedia} from "react-icons/md";
+import {CiFaceSmile} from "react-icons/ci";
+import {ChangeEvent, useEffect, useRef, useState} from "react";
 
 function UserHomePage() {
+
+    const [textAreaValue, setTextAreaValue] = useState('')
+
+    const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setTextAreaValue(e.target.value)
+    }
+
+    // Dynamic change textarea height based on the text long
+    const textAreaRef = useRef<HTMLTextAreaElement>(null)
+
+    useEffect( () => {
+        if (textAreaRef.current) {
+            textAreaRef.current.style.height = 'auto';
+            textAreaRef.current.style.height = textAreaRef.current.scrollHeight + 1 + 'px';
+        }
+    }, [textAreaValue] )
+
     return (
         <div className={`bg-black w-screen h-screen flex justify-center`}>
-            <div className={`container grid grid-cols-[1fr,3fr,1fr]`}>
+            <div className={`container grid grid-cols-[1fr,3fr,2fr]`}>
                 <Sidebar/>
+
+                <div className={`text-neutral-100 border border-t-0 border-zinc-700/70`}>
+                    <header className={`w-full grid grid-cols-2 border-b border-zinc-700/70`}>
+                        <button className={`bg-transparent hover:bg-neutral-600/30 py-3`}>For you</button>
+                        <button className={`bg-transparent hover:bg-neutral-600/30 py-3`}>Following</button>
+                    </header>
+
+                    {/* Post Section */}
+                    <div className={`flex flex-col py-3 px-6 border-b border-zinc-700/70`}>
+                        <div className={`flex gap-x-3`}>
+                            <div className={`text-4xl`}>
+                                <FaRegCircleUser />
+                            </div>
+                            <div className={`flex flex-wrap w-full gap-y-5`}>
+                                <textarea
+                                    ref={textAreaRef}
+                                    maxLength={280}
+                                    onChange={handleChange}
+                                    placeholder={`What is happening?!`}
+                                    name={`textAreaValue`}
+                                    value={textAreaValue}
+                                    className={`bg-transparent overflow-x-auto resize-none border-b border-zinc-700/70 text-xl w-full pt-1 pb-3 placeholder:font-light placeholder:text-neutral-500 focus:outline-0`}
+                                />
+
+                                <div className={`flex justify-between w-full`}>
+                                    <div className={`flex text-2xl text-sky-600`}>
+                                        <div className={`bg-transparent hover:bg-sky-600/20 p-2 rounded-full cursor-pointer`}>
+                                            <MdOutlinePermMedia />
+                                        </div>
+                                        <div className={`bg-transparent hover:bg-sky-600/20 p-2 rounded-full cursor-pointer`}>
+                                            <CiFaceSmile />
+                                        </div>
+                                    </div>
+
+                                    <div className={`bg-sky-600 px-6 flex justify-center items-center rounded-full cursor-pointer`}>
+                                        Post
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/partials/Sidebar.tsx
+++ src/components/partials/Sidebar.tsx
@@ -1,49 +1,67 @@
 import {Link} from "react-router-dom";
 import {MdHomeFilled} from "react-icons/md";
-import {FaXTwitter} from "react-icons/fa6";
-import {HiMagnifyingGlass} from "react-icons/hi2";
+import {FaRegCircleUser, FaXTwitter} from "react-icons/fa6";
+import {HiMiniMagnifyingGlass} from "react-icons/hi2";
 import {BsBell} from "react-icons/bs";
 import {LuUser} from "react-icons/lu";
+import {HiOutlineDotsHorizontal} from "react-icons/hi";
 
 function Sidebar() {
     return (
-        <div>
-            <ul className={`text-neutral-100 bg-sky-600 px-2 py-3 h-full flex flex-col gap-y-6`}>
-                <li>
-                    <Link className={``} to={`/home`}>
-                        <FaXTwitter className={`size-8`}/>
+        <div className={`text-neutral-100 px-6 pb-5 pt-1 h-full flex flex-col justify-between`}>
+            <ul className={`flex flex-col gap-y-5`}>
+                <li className={`flex items-center`}>
+                    <Link to={`/home`}>
+                        <div className={`hover:bg-neutral-600/30 rounded-full p-3`}>
+                            <FaXTwitter className={`size-8`}/>
+                        </div>
                     </Link>
                 </li>
-                <li>
-                    <Link className={`flex`} to={`/home`}>
+                <li className={``}>
+                    <Link className={`flex items-end gap-x-4 text-2xl hover:bg-neutral-600/30 rounded-full w-fit pr-7 pl-3 py-3 transition`} to={`/home`}>
                         <MdHomeFilled className={`size-8`}/>
                         Home
                     </Link>
                 </li>
                 <li>
-                    <Link className={`flex`} to={`/explore`}>
-                        <HiMagnifyingGlass className={`size-8`}/>
+                    <Link className={`flex items-end gap-x-4 text-2xl hover:bg-neutral-600/30 rounded-full w-fit pr-7 pl-3 py-3 transition`} to={`/explore`}>
+                        <HiMiniMagnifyingGlass className={`size-8`}/>
                         Explore
                     </Link>
                 </li>
                 <li>
-                    <Link className={`flex`} to={`/notifications`}>
+                    <Link className={`flex items-end gap-x-4 text-2xl hover:bg-neutral-600/30 rounded-full w-fit pr-7 pl-3 py-3 transition`} to={`/notifications`}>
                         <BsBell className={`size-8`}/>
                         Notifications
                     </Link>
                 </li>
                 <li>
-                    <Link className={`flex`} to={`/profile`}>
+                    <Link className={`flex items-end gap-x-4 text-2xl hover:bg-neutral-600/30 rounded-full w-fit pr-7 pl-3 py-3 transition`} to={`/profile`}>
                         <LuUser className={`size-8`}/>
                         Profile
                     </Link>
                 </li>
                 <li>
-                    <Link className={`flex`} to={`/create-post`}>
+                    <Link className={`flex items-end justify-center py-3 rounded-full gap-x-4 font-semibold bg-sky-500 text-xl hover:bg-sky-600 transition`} to={`/create-post`}>
                         Post
                     </Link>
                 </li>
             </ul>
+            <div className={`flex items-center gap-x-9 hover:bg-neutral-600/30 rounded-full px-4 py-2 transition cursor-pointer`}>
+                <div className={`flex items-center gap-x-4`}>
+                    <div className={`w-1/3 text-4xl`}>
+                        <FaRegCircleUser />
+                    </div>
+                    <div>
+                        <div className={`font-semibold`}>Mohamed Ashour</div>
+                        <div className={`text-neutral-500`}>@MohamedAsh119</div>
+                    </div>
+                </div>
+
+                <div>
+                    <HiOutlineDotsHorizontal />
+                </div>
+            </div>
         </div>
     )
 }
