c67ca6ec7eb1b8cf1f0f21aafcffdd3be43974ea
Mohamed Ashour <boxv38145@gmail.com>
2024-02-20 03:43:49 +0200

    Logout

--- src/components/pages/UserHomePage.tsx
+++ src/components/pages/UserHomePage.tsx
@@ -36,16 +36,16 @@ function UserHomePage() {
                     <LuArrowBigUp className={`size-7 text-white/90`}/>
                 </div>
 
-                <div className={`justify-end hidden sm:flex`}>
+                <div className={`justify-end hidden sm:flex relative`}>
                     <Sidebar/>
                 </div>
 
-                <div className={`text-neutral-100 border border-t-0 border-zinc-700/70 w-full relative`}>
+                <div className={`text-neutral-100 border border-t-0 border-zinc-700/70 w-full relative animate-slide-down`}>
                         <header className={`w-full grid grid-cols-1 border-b border-zinc-700/70 fixed 2xl:max-w-[38.46rem] xl:max-w-[33.3rem] lg:max-w-[33.7rem] md:max-w-[39.34rem] sm:max-w-[31.2rem] xs:max-w-[31.15rem] xxs:max-w-[27.6rem] backdrop-blur-md`}>
                             <div className={`flex sm:hidden justify-between px-6 py-5 pb-1`}>
-                                <FaRegCircleUser className={`size-10`}/>
-                                <FaXTwitter className={`size-10`}/>
-                                <IoSettingsOutline className={`size-10 text-white`}/>
+                                <FaRegCircleUser className={`size-9`}/>
+                                <FaXTwitter className={`size-9`}/>
+                                <IoSettingsOutline className={`size-9`}/>
                             </div>
                             <div className={`w-full`}>
                                 <button className={`hover:bg-neutral-600/30 py-4 w-1/2 transition`}>For you</button>
@@ -54,7 +54,7 @@ function UserHomePage() {
                         </header>
 
                     {/* Post Section */}
-                    <div className={`flex flex-col py-3 px-6 sm:mt-16 mt-36 border-b border-zinc-700/70`}>
+                    <div className={`flex flex-col py-3 px-6 sm:mt-16 mt-36 border-b border-zinc-700/70 z-10`}>
                         <div className={`flex gap-x-3`}>
 
                             <FaRegCircleUser className={`size-10`}/>
--- src/components/partials/Sidebar.tsx
+++ src/components/partials/Sidebar.tsx
@@ -5,38 +5,77 @@ import {HiMiniMagnifyingGlass} from "react-icons/hi2";
 import {BsBell} from "react-icons/bs";
 import {LuUser} from "react-icons/lu";
 import {HiOutlineDotsHorizontal} from "react-icons/hi";
+import {useContext, useEffect, useRef, useState} from "react";
+import {AppContext} from "../appContext/AppContext.tsx";
+import ApiClient from "../services/ApiClient.tsx";
 
 function Sidebar() {
+
+    const {setUser} = useContext(AppContext)
+
+    const [logoutWindowOpen, setLogoutWindowOpen] = useState(false)
+    const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.stopPropagation()
+        setLogoutWindowOpen(!logoutWindowOpen)
+    }
+
+    // Logout function
+    const logout = () => {
+        if(localStorage.getItem('token')){
+            ApiClient().get('/logout')
+                .then( () => {
+                    localStorage.removeItem('token')
+                    setUser(null)
+                })
+                .catch(() => {})
+        }
+    }
+
+    // Handle click outside the logout window
+    const logoutRef = useRef<HTMLDivElement>(null)
+    useEffect( () => {
+        const handleOutside = (e: MouseEvent) => {
+            if(!logoutRef.current?.contains(e.target as Node)){
+                setLogoutWindowOpen(false)
+            }
+        }
+        document.addEventListener('mousedown', handleOutside)
+        return () => {
+            document.removeEventListener('mousedown', handleOutside)
+        }
+
+    }, [] )
+
     return (
-        <div className={`text-neutral-100 xl:px-16 px-4 pb-5 pt-1 h-dvh flex flex-col justify-between container fixed max-w-fit`}>
+        <div className={`text-neutral-100 xl:px-16 px-4 pb-5 pt-1 h-dvh flex flex-col justify-between container z-50 fixed max-w-fit animate-slide-right`}>
             <ul className={`flex flex-col gap-y-5`}>
                 <li className={`flex items-center`}>
                     <Link to={`/home`}>
-                        <div className={`hover:bg-neutral-600/30 rounded-full p-3`}>
+                        <div className={`hover:bg-neutral-600/30 rounded-full p-3 `}>
                             <FaXTwitter className={`size-8`}/>
                         </div>
                     </Link>
                 </li>
                 <li className={``}>
-                    <Link className={`flex items-end gap-x-4 text-xl hover:bg-neutral-600/30 rounded-full w-fit pr-7 pl-3 py-3 transition`} to={`/home`}>
+                    <Link className={`flex items-end gap-x-4 text-xl hover:bg-neutral-600/30 rounded-full w-fit xl:pr-7 xl:pl-3 xl:py-3 p-3 transition`} to={`/home`}>
                         <MdHomeFilled className={`size-8`}/>
                         <span className={`hidden xl:block`}>Home</span>
                     </Link>
                 </li>
                 <li>
-                    <Link className={`flex items-end gap-x-4 text-xl hover:bg-neutral-600/30 rounded-full w-fit pr-7 pl-3 py-3 transition`} to={`/explore`}>
+                    <Link className={`flex items-end gap-x-4 text-xl hover:bg-neutral-600/30 rounded-full w-fit xl:pr-7 xl:pl-3 xl:py-3 p-3  transition`} to={`/explore`}>
                         <HiMiniMagnifyingGlass className={`size-8`}/>
                         <span className={`hidden xl:block`}>Explore</span>
                     </Link>
                 </li>
                 <li>
-                    <Link className={`flex items-end gap-x-4 text-xl hover:bg-neutral-600/30 rounded-full w-fit pr-7 pl-3 py-3 transition`} to={`/notifications`}>
+                    <Link className={`flex items-end gap-x-4 text-xl hover:bg-neutral-600/30 rounded-full w-fit xl:pr-7 xl:pl-3 xl:py-3 p-3  transition`} to={`/notifications`}>
                         <BsBell className={`size-8`}/>
                         <span className={`hidden xl:block`}>Notifications</span>
                     </Link>
                 </li>
                 <li>
-                    <Link className={`flex items-end gap-x-4 text-xl hover:bg-neutral-600/30 rounded-full w-fit pr-7 pl-3 py-3 transition`} to={`/profile`}>
+                    <Link className={`flex items-end gap-x-4 text-xl hover:bg-neutral-600/30 rounded-full w-fit xl:pr-7 xl:pl-3 xl:py-3 p-3  transition`} to={`/profile`}>
                         <LuUser className={`size-8`}/>
                         <span className={`hidden xl:block`}>Profile</span>
                     </Link>
@@ -48,7 +87,15 @@ function Sidebar() {
                     </Link>
                 </li>
             </ul>
-            <div className={`flex items-center gap-x-9 hover:bg-neutral-600/30 rounded-full px-4 py-2 transition cursor-pointer`}>
+
+            {/*Logout window*/}
+            <div className={`bg-black flex flex-col gap-y-3 border border-neutral-700/70 py-4 px-4 rounded-lg absolute w-[21rem] bottom-24 shadow-[-2px_2px_12px_#4f4e4e] ${logoutWindowOpen ? 'appear' : 'disappear'}`}>
+                <button disabled={!logoutWindowOpen} className={`bg-neutral-950 py-3 px-6 text-left rounded-lg hover:bg-neutral-800 transition ${!logoutWindowOpen ? 'cursor-default' : 'cursor-pointer'}`}>Settings</button>
+                <button disabled={!logoutWindowOpen} onClick={logout} className={`bg-neutral-950 py-3 px-6 text-left rounded-lg hover:bg-neutral-800 transition ${!logoutWindowOpen ? 'cursor-default' : 'cursor-pointer'}`}>Logout @MohamedAshour</button>
+            </div>
+
+
+            <div ref={logoutRef} onClick={handleClick} className={`flex items-center gap-x-9 xl:hover:bg-neutral-600/30 rounded-full px-4 py-2 transition cursor-pointer`}>
                 <div className={`flex items-center gap-x-4`}>
 
                     <FaRegCircleUser className={`size-10`}/>
