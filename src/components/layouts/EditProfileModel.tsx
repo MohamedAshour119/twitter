c6da0b583c70733760cdeab957b40613a31cdfdd
Mohamed Ashour <boxv38145@gmail.com>
2024-04-20 20:56:35 +0200

    Edit user info in profile page

--- src/components/layouts/EditProfileModel.tsx
+++ src/components/layouts/EditProfileModel.tsx
@@ -2,6 +2,7 @@ import {HiMiniXMark} from "react-icons/hi2";
 import {TbCameraPlus} from "react-icons/tb";
 import {Dispatch, MouseEventHandler, SetStateAction, useContext, useEffect, useRef} from "react";
 import {AppContext} from "../appContext/AppContext.tsx";
+import Select from "react-select";
 
 interface Props {
     setIsShowEditInfoModel: Dispatch<SetStateAction<boolean>>
@@ -40,20 +41,33 @@ function EditProfileModel(props: Props) {
                 <button className={`mx-2 bg-white hover:bg-zinc-200 transition text-black px-5 py-2 rounded-full`}>Save</button>
             </div>
         {/*  Cover section  */}
-            <div className={`my-32`}>
-                <div className={`bg-gray-600`}>
-                    <div className={`bg-neutral-950 hover:bg-neutral-900 text-2xl flex justify-center items-center rounded-full h-9 w-9 transition`}>
+            <div className={`my-8 relative`}>
+                <div className={`bg-gray-600 rounded h-48 relative`}>
+                    <label htmlFor={`upload_cover`} className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-neutral-950 cursor-pointer hover:bg-neutral-900 text-2xl flex justify-center items-center rounded-full h-9 w-9 transition`}>
                         <TbCameraPlus />
-                    </div>
+                        <input
+                            id={`upload_cover`}
+                            type="file"
+                            className={`hidden`}
+                        />
+                    </label>
                 </div>
             {/*  Avatar  */}
-                <div>
+                <div className={`absolute top-2/3 left-2`}>
                     <img src={`${baseUrl}/storage/${user?.avatar}`} alt=""
-                         className={`object-cover w-32 h-32 rounded-full ${!user ? 'invisible' : ''}`}/>
+                         className={`object-cover w-32 h-32 rounded-full brightness-75 ${!user ? 'invisible' : ''}`}/>
+                    <label htmlFor={`upload_cover`} className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-neutral-950 cursor-pointer hover:bg-neutral-900 text-2xl flex justify-center items-center rounded-full h-9 w-9 transition`}>
+                        <TbCameraPlus />
+                        <input
+                            id={`upload_cover`}
+                            type="file"
+                            className={`hidden`}
+                        />
+                    </label>
                 </div>
             </div>
         {/*  More info  */}
-            <div>
+            <div className={`mt-24 flex flex-col gap-y-3`}>
                 <input
                     maxLength={50}
                     name={`display_name`}
@@ -66,24 +80,49 @@ function EditProfileModel(props: Props) {
                     autoComplete="one-time-code"
                 />
 
-                <input
+                <textarea
                     maxLength={160}
                     name={`bio`}
                     // value={userCredentials?.username}
                     // onChange={handleInputsChange}
-                    className={`h-14 w-full border border-zinc-600 focus:placeholder:text-sky-600 ring-sky-600 focus:border-sky-600 rounded bg-transparent px-3 placeholder:text-zinc-500 placeholder:absolute focus:outline-0 focus:ring-1 `}
-                    type="text"
+                    className={`h-14 w-full min-h-32 max-h-40 border border-zinc-600 focus:placeholder:text-sky-600 ring-sky-600 focus:border-sky-600 rounded bg-transparent px-3 placeholder:mt-4 placeholder:text-zinc-500 placeholder:absolute focus:outline-0 focus:ring-1 `}
                     placeholder="Bio"
                     // disabled={isLoading}
                     autoComplete="one-time-code"
                 />
 
             {/*  Birth date  */}
-                <div>
+                <div className={`text-zinc-500`}>
                     <div>
-                        Birth date .<span>Edit</span>
+                        Birth date
                     </div>
-                    <div>September 11, 2001</div>
+                    <div className={`text-neutral-200 text-xl`}>September 11, 2001</div>
+
+                    <div className={`grid grid-cols-1 md:grid-cols-[2fr,1fr,1fr] gap-y-4 md:gap-y-0 gap-x-3 mt-3`}>
+                        <Select
+                            options={months}
+                            isDisabled={isLoading}
+                            placeholder={'Month'}
+                            onChange={handleSelectedMonthChange}
+                            styles={styles}
+                        />
+                        <Select
+                            options={days}
+                            isDisabled={isLoading}
+                            placeholder={'Day'}
+                            noOptionsMessage={() => 'Select Month'}
+                            onChange={handleDaySelectedChange}
+                            styles={styles}
+                        />
+
+                        <Select
+                            options={years}
+                            isDisabled={isLoading}
+                            placeholder={'Year'}
+                            onChange={handleYearSelectedChange}
+                            styles={styles}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
